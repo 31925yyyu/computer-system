7f529074b0729f045494b8efe089ba96e669bf40
Yvette_csyyyu <99969129+31925yyyu@users.noreply.github.com>
2022-04-09 17:31:12 +0200

    Qestion5.sh

---
+++ Homework2/wordsinwords.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Write shell script program (wordsinwords.sh) which has got a parameter (a filename)
+#The file contains a text. 
+#The task of the script is to decide which word is contained most frequently in other words.
+
+if [ $# -eq 1 ]
+then
+	filename=$1
+else
+	echo "Please give the file name"
+	read filename
+fi
+
+if [ ! -f $1 ]
+then
+	echo "The file doesn't exit"
+	exit
+fi
+
+count=0
+
+while read a
+do
+	for i in $a
+	do
+		content=`grep -o -i $i $1 | wc -l`
+		if [ $content -gt $count ]
+		then
+			count=$content
+			winner=$i
+		fi
+	done
+done < $1
+
+echo "$winner is the game winner"
+
